c593cd6693ca21b360ed1bd0f533736635db7cf1
Hussein Khaled <husseinkhaled733@gmail.com>
2023-04-20 00:23:05 +0200

    implementing bellman-ford

--- src/CreateGraphs/Graph.java
+++ src/CreateGraphs/Graph.java
@@ -2,6 +2,7 @@ package src.CreateGraphs;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Graph {
@@ -30,9 +31,36 @@ public class Graph {
     }
     public void print()
     {
-        System.out.println("Verices="+V+", Edges="+E);
+        System.out.println("Vertices="+V+", Edges="+E);
         for (int i = 0; i < E; i++) {
             System.out.println(edges[i].getFrom()+"->"+edges[i].getTo()+","+edges[i].getW());
         }
     }
+    public boolean bellmanFord(int src,int[] cost,int[] p){
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        Arrays.fill(p,-1);
+        cost[src] = 0;
+        for (int i = 0; i <V-1 ; i++) {
+            for (int j = 0; j <E ; j++) {
+                int u = edges[j].getFrom();
+                int v = edges[j].getTo();
+                int w = edges[j].getW();
+                if (cost[u]!=Integer.MAX_VALUE && cost[u]+w<cost[v]){
+                    cost[v] = Math.max(-Integer.MAX_VALUE,cost[u]+w);//for overflow
+                    p[v] = u;
+                }
+            }
+        }
+        //check negative cycle
+        for (int i = 0; i <E ; i++) {
+            int u = edges[i].getFrom();
+            int v = edges[i].getTo();
+            int w = edges[i].getW();
+            if (cost[u]!=Integer.MAX_VALUE && cost[u]+w<cost[v]){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ import src.CreateGraphs.Graph;
 
 public class Main {
     public static void main(String[] args) {
-        Graph g=new Graph("/home/mahmoud/graph");
+        Graph g = new Graph("/home/exception/graph");
         g.print();
+        int[] cost = new int[g.size()];
+        int[] p = new int[g.size()];
+        if (g.bellmanFord(0, cost, p)) {
+            System.out.println("No negative cycle");
+        }
+        for (int i = 0; i < g.size(); i++) {
+            System.out.println("cost[" + i + "]=" + cost[i]+", p["+i+"]="+p[i]);
+        }
     }
 }
