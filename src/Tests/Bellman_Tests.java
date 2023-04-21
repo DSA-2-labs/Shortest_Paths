c145686957d1e025d162cd9c3e389c04a46c34e0
mahmoud Ghlab <randyorten9055@gmail.com>
2023-04-21 03:20:12 +0200

    Tests

---
+++ src/Tests/Bellman_Tests.java
@@ -0,0 +1,84 @@
+package src.Tests;
+import org.junit.Test;
+import src.CreateGraphs.Graph;
+
+import static org.junit.Assert.*;
+
+public class Bellman_Tests {
+    private Graph graph;
+    @Test
+    public void Test1()
+    {
+        graph=new Graph("/home/mahmoud/Test1");
+    }
+    @Test
+    public void Test2()
+    {
+        graph=new Graph("/home/mahmoud/Test2");
+    }
+    @Test
+    public void Test3()
+    {
+        graph=new Graph("/home/mahmoud/Test3");
+    }
+    @Test
+    public void Test4()
+    {
+        graph=new Graph("/home/mahmoud/Test4");
+    }
+    @Test
+    public void Test5()
+    {
+        graph=new Graph("/home/mahmoud/Test5");
+    }
+    @Test
+    public void Test6()
+    {
+        graph=new Graph("/home/mahmoud/Test6");
+    }
+    @Test
+    public void Test7()
+    {
+        graph=new Graph("/home/mahmoud/Test7");
+    }
+    @Test
+    public void Test8()
+    {
+        graph=new Graph("/home/mahmoud/Test8");
+    }
+    @Test
+    public void Test9()
+    {
+        graph=new Graph("/home/mahmoud/Test9");
+    }
+    @Test
+    public void Test10()
+    {
+        graph=new Graph("/home/mahmoud/Test10");
+    }
+    @Test
+    public void Test11()
+    {
+        graph=new Graph("/home/mahmoud/Test11");
+    }
+    @Test
+    public void Test12()
+    {
+        graph=new Graph("/home/mahmoud/Test12");
+    }
+    @Test
+    public void Test13()
+    {
+        graph=new Graph("/home/mahmoud/Test13");
+    }
+    @Test
+    public void Test14()
+    {
+        graph=new Graph("/home/mahmoud/Test14");
+    }
+    @Test
+    public void Test15()
+    {
+        graph=new Graph("/home/mahmoud/Test15");
+    }
+}
