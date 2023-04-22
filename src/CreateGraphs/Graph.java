45cd7295e915d125aeb68777c3c316668f176969
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-04-22 05:10:16 +0200

    Added Implementation of dijkestra

--- src/CreateGraphs/Graph.java
+++ src/CreateGraphs/Graph.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Graph {
     private int V,E;
     private Edge[] edges;
+    private int[][] adjMatrix = new int[V][V];
     public Graph(String filename)
     {
         try {
@@ -17,6 +18,12 @@ public class Graph {
             edges=new Edge[E];
             for (int i = 0; i < E; i++) {
                 edges[i]=new Edge(scan.nextInt(),scan.nextInt(),scan.nextInt());
+                int x = edges[i].getFrom();
+                int y = edges[i].getTo();
+                int w = edges[i].getW();
+                adjMatrix[x][y] = w;
+                adjMatrix[x][x] = 0;
+                adjMatrix[y][y] = 0;
             }
             scan.close();
         }
@@ -62,5 +69,35 @@ public class Graph {
         }
         return true;
     }
+    public void dijkestra(int src, int[] cost, int[] p){
+        boolean[] added = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            cost[i] = Integer.MAX_VALUE;
+            added[i] = false;
+        }
+        cost[src] = 0;
+        p[src] = -1;
+        for(int i=0 ; i<V ; i++){
+            int nearestV = -1;
+            int shortest_D = Integer.MAX_VALUE;
+            for(int j=0 ; j<V ; j++){
+                if(!added[j] && cost[j] < shortest_D){
+                    shortest_D = cost[j];
+                    nearestV = j;
+                }
+            }
+            added[nearestV] = true;
+            for(int index = 0; index<V ; i++){
+                int edgeW = adjMatrix[nearestV][index];
+                if(edgeW > 0 && shortest_D+edgeW < cost[index]){
+                    cost[index] = shortest_D+edgeW;
+                    p[index] = nearestV;
+                }
+
+            }
+
+        }
+    }
+
 
 }
