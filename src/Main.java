0b08c5d0c733bf82d8a2cd01b4570676c9b1131e
mahmoud Ghlab <randyorten9055@gmail.com>
2023-04-22 19:42:41 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ import src.CreateGraphs.Graph;
 
 public class Main {
     public static void main(String[] args) {
-        Graph g = new Graph("/home/exception/graph");
+        Graph g = new Graph("/home/mahmoud/Test1.txt");
         g.print();
-        int[] cost = new int[g.size()];
-        int[] p = new int[g.size()];
-        if (g.bellmanFord(0, cost, p)) {
+        int[][] cost = new int[g.size()][g.size()];
+        int[][] p = new int[g.size()][g.size()];
+        /*if (g.bellmanFord(0, cost, p)) {
             System.out.println("No negative cycle");
         }
         for (int i = 0; i < g.size(); i++) {
             System.out.println("cost[" + i + "]=" + cost[i]+", p["+i+"]="+p[i]);
-        }
+        }*/
+        System.out.println(g.Floyd_warshall(cost,p));
     }
+
 }
