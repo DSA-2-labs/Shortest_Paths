a4615e45eef2a94d4dcde5abe27c6761362b958d
mahmoud Ghlab <randyorten9055@gmail.com>
2023-04-22 16:04:04 +0200

    Tests

--- src/Tests/Floyd_Tests.java
+++ src/Tests/Floyd_Tests.java
@@ -6,79 +6,81 @@ import static org.junit.Assert.*;
 
 public class Floyd_Tests {
     private Graph graph;
+    private int[] cost;
+    private int[] parents;
     @Test
     public void Test1()
     {
-        graph=new Graph("/home/mahmoud/Test1");
+        graph=new Graph("/home/mahmoud/Test1.txt");
     }
     @Test
     public void Test2()
     {
-        graph=new Graph("/home/mahmoud/Test2");
+        graph=new Graph("/home/mahmoud/Test2.txt");
     }
     @Test
     public void Test3()
     {
-        graph=new Graph("/home/mahmoud/Test3");
+        graph=new Graph("/home/mahmoud/Test3.txt");
     }
     @Test
     public void Test4()
     {
-        graph=new Graph("/home/mahmoud/Test4");
+        graph=new Graph("/home/mahmoud/Test4.txt");
     }
     @Test
     public void Test5()
     {
-        graph=new Graph("/home/mahmoud/Test5");
+        graph=new Graph("/home/mahmoud/Test5.txt");
     }
     @Test
     public void Test6()
     {
-        graph=new Graph("/home/mahmoud/Test6");
+        graph=new Graph("/home/mahmoud/Test6.txt");
     }
     @Test
     public void Test7()
     {
-        graph=new Graph("/home/mahmoud/Test7");
+        graph=new Graph("/home/mahmoud/Test7.txt");
     }
     @Test
     public void Test8()
     {
-        graph=new Graph("/home/mahmoud/Test8");
+        graph=new Graph("/home/mahmoud/Test8.txt");
     }
     @Test
     public void Test9()
     {
-        graph=new Graph("/home/mahmoud/Test9");
+        graph=new Graph("/home/mahmoud/Test9.txt");
     }
     @Test
     public void Test10()
     {
-        graph=new Graph("/home/mahmoud/Test10");
+        graph=new Graph("/home/mahmoud/Test10.txt");
     }
     @Test
     public void Test11()
     {
-        graph=new Graph("/home/mahmoud/Test11");
+        graph=new Graph("/home/mahmoud/Test11.txt");
     }
     @Test
     public void Test12()
     {
-        graph=new Graph("/home/mahmoud/Test12");
+        graph=new Graph("/home/mahmoud/Test12.txt");
     }
     @Test
     public void Test13()
     {
-        graph=new Graph("/home/mahmoud/Test13");
+        graph=new Graph("/home/mahmoud/Test13.txt");
     }
     @Test
     public void Test14()
     {
-        graph=new Graph("/home/mahmoud/Test14");
+        graph=new Graph("/home/mahmoud/Test14.txt");
     }
     @Test
     public void Test15()
     {
-        graph=new Graph("/home/mahmoud/Test15");
+        graph=new Graph("/home/mahmoud/Test15.txt");
     }
 }
