eafb1d307a1515e9a16aca3fd623d474eeafd28c
Mohamed <mo.hassan2792@gmail.com>
2023-04-30 00:18:54 +0300

    update

--- src/CreateGraphs/Graph.java
+++ src/CreateGraphs/Graph.java
@@ -6,27 +6,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Graph {
-    private int V,E;
+    private static final int INF = Integer.MAX_VALUE;
+    private int V, E;
     private Edge[] edges;
     private int[][] adjMatrix;
 
-
-
-
-
-
-
-
-    public Graph(String filename)
-    {
+    public Graph(String filename) {
         try {
-            Scanner scan=new Scanner(new File(filename));
-            V=scan.nextInt();
-            E= scan.nextInt();
-            edges=new Edge[E];
-            adjMatrix= new int[V][V];
+            Scanner scan = new Scanner(new File(filename));
+            V = scan.nextInt();
+            E = scan.nextInt();
+            edges = new Edge[E];
+            adjMatrix = new int[V][V];
             for (int i = 0; i < E; i++) {
-                edges[i]=new Edge(scan.nextInt(),scan.nextInt(),scan.nextInt());
+                edges[i] = new Edge(scan.nextInt(), scan.nextInt(), scan.nextInt());
                 int x = edges[i].getFrom();
                 int y = edges[i].getTo();
                 int w = edges[i].getW();
@@ -35,122 +28,109 @@ public class Graph {
                 adjMatrix[y][y] = 0;
             }
             scan.close();
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    public Graph() {
+        Scanner input = new Scanner(System.in);
 
-
-
-
-
-
-
-
-
-
-    public Graph()
-    {  Scanner input = new Scanner(System.in);
-       
-        while(true)
-        { int Error=0;
+        while (true) {
+            int Error = 0;
             System.out.print("Enter the path of the File that contain the Graph : ");
-          String s = input.nextLine();
-        try {
-            Scanner scan=new Scanner(new File(s));
-            V=scan.nextInt();
-            E= scan.nextInt();
-            edges=new Edge[E];
-            adjMatrix= new int[V][V];
-            for (int i = 0; i <V ; i++) {
-                Arrays.fill(adjMatrix[i],Integer.MAX_VALUE);
+            String s = input.nextLine();
+            try {
+                Scanner scan = new Scanner(new File(s));
+                V = scan.nextInt();
+                E = scan.nextInt();
+                edges = new Edge[E];
+                adjMatrix = new int[V][V];
+                for (int i = 0; i < V; i++) {
+                    Arrays.fill(adjMatrix[i], INF);
+                }
+                for (int i = 0; i < E; i++) {
+                    edges[i] = new Edge(scan.nextInt(), scan.nextInt(), scan.nextInt());
+                    int x = edges[i].getFrom();
+                    int y = edges[i].getTo();
+                    int w = edges[i].getW();
+                    adjMatrix[x][y] = w;
+                    adjMatrix[x][x] = 0;
+                    adjMatrix[y][y] = 0;
+                    Error = 0;
+                }
+                scan.close();
+            } catch (FileNotFoundException e) {
+                System.out.print("\nOops Wrong path \n");
+                Error = 1;
             }
-            for (int i = 0; i < E; i++) {
-                edges[i]=new Edge(scan.nextInt(),scan.nextInt(),scan.nextInt());
-                int x = edges[i].getFrom();
-                int y = edges[i].getTo();
-                int w = edges[i].getW();
-                adjMatrix[x][y] = w;
-                adjMatrix[x][x] = 0;
-                adjMatrix[y][y] = 0;
-                Error=0;
+            if (Error == 0) {
+                break;
             }
-            scan.close();
         }
-        catch (FileNotFoundException e)
-        {
-            System.out.print("\nOops Wrong path \n"); 
-            Error=1;
-
-        } 
-        if(Error==0){
-            break;
-        }
-    }
         print();
     }
-    public int size()
-    {
+
+    public int size() {
         return V;
     }
-    public void print()
-    {
-        System.out.println("Vertices="+V+", Edges="+E);
+
+    public void print() {
+        System.out.println("Vertices=" + V + ", Edges=" + E);
         for (int i = 0; i < E; i++) {
-            System.out.println(edges[i].getFrom()+"->"+edges[i].getTo()+","+edges[i].getW());
+            System.out.println(edges[i].getFrom() + "->" + edges[i].getTo() + "," + edges[i].getW());
         }
     }
-    public boolean bellmanFord(int src,int[] cost,int[] p){
-        Arrays.fill(cost,Integer.MAX_VALUE);
-        Arrays.fill(p,-1);
+
+    public boolean bellmanFord(int src, int[] cost, int[] p) {
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        Arrays.fill(p, -1);
         cost[src] = 0;
-        for (int i = 0; i <V-1 ; i++) {
-            for (int j = 0; j <E ; j++) {
+        for (int i = 0; i < V - 1; i++) {
+            for (int j = 0; j < E; j++) {
                 int u = edges[j].getFrom();
                 int v = edges[j].getTo();
                 int w = edges[j].getW();
-                if (cost[u]!=Integer.MAX_VALUE && cost[u]+w<cost[v]){
-                    cost[v] = Math.max(-Integer.MAX_VALUE,cost[u]+w);//for overflow
+                if (cost[u] != INF && cost[u] + w < cost[v]) {
+                    cost[v] = Math.max(-INF, cost[u] + w);//for overflow
                     p[v] = u;
                 }
             }
         }
         //check negative cycle
-        for (int i = 0; i <E ; i++) {
+        for (int i = 0; i < E; i++) {
             int u = edges[i].getFrom();
             int v = edges[i].getTo();
             int w = edges[i].getW();
-            if (cost[u]!=Integer.MAX_VALUE && cost[u]+w<cost[v]){
+            if (cost[u] != INF && cost[u] + w < cost[v]) {
                 return false;
             }
         }
         return true;
     }
-    public void dijkestra(int src, int[] cost, int[] p){
+
+    public void dijkestra(int src, int[] cost, int[] p) {
         boolean[] added = new boolean[V];
         for (int i = 0; i < V; i++) {
-            cost[i] = Integer.MAX_VALUE;
+            cost[i] = INF;
             added[i] = false;
         }
         cost[src] = 0;
         p[src] = -1;
-        for(int i=0 ; i<V ; i++){
+        for (int i = 0; i < V; i++) {
             int nearestV = -1;
-            int shortest_D = Integer.MAX_VALUE;
-            for(int j=0 ; j<V ; j++){
-                if(!added[j] && cost[j] < shortest_D){
+            int shortest_D = INF;
+            for (int j = 0; j < V; j++) {
+                if (!added[j] && cost[j] < shortest_D) {
                     shortest_D = cost[j];
                     nearestV = j;
                 }
             }
             added[nearestV] = true;
-            for(int index = 0; index<V ; index++){
+            for (int index = 0; index < V; index++) {
                 int edgeW = adjMatrix[nearestV][index];
-                if(edgeW<Integer.MAX_VALUE && shortest_D+edgeW < cost[index]){
-                    cost[index] = shortest_D+edgeW;
+                if (edgeW < Integer.MAX_VALUE && shortest_D + edgeW < cost[index]) {
+                    cost[index] = shortest_D + edgeW;
                     p[index] = nearestV;
                 }
 
@@ -159,42 +139,40 @@ public class Graph {
         }
     }
 
-    public boolean Floyd_warshall(int[][] costs,int[][] predecessors){
-        for (int i = 0; i < predecessors.length; i++) {
-            Arrays.fill(predecessors[i],Integer.MAX_VALUE);
-            predecessors[i][i]=0;
+    public boolean Floyd_warshall(int[][] costs, int[][] predecessors) {
+        for (int i = 0; i < V; i++) {
+            Arrays.fill(costs[i], INF);
+            Arrays.fill(predecessors[i], -1);
+            costs[i][i] = 0;
         }
-        for (int i = 0; i < E; i++) {
-            predecessors[edges[i].getFrom()][edges[i].getTo()]=edges[i].getW();
+        for (Edge edge : edges) {
+            costs[edge.getFrom()][edge.getTo()] = edge.getW();
+            predecessors[edge.getFrom()][edge.getTo()] = edge.getFrom();
         }
-        costs=predecessors;
-        for (int k = 0; k < adjMatrix.length; k++) {
-            for (int i = 0; i < adjMatrix.length; i++) {
-                for (int j = 0; j < adjMatrix.length; j++) {
-                    if (costs[i][k] != Integer.MAX_VALUE && costs[k][j] != Integer.MAX_VALUE)//for overflow
-                    {
-                        costs[i][j]=Math.min(costs[i][j],costs[i][k]+costs[k][j]);
-                        if (costs[k][j] < Integer.MAX_VALUE && costs[j][j] < 0 && costs[j][i] < Integer.MAX_VALUE)
-                            costs[k][i] = Integer.MIN_VALUE;
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (costs[i][k] != INF && costs[k][j] != INF && costs[i][j] > costs[i][k] + costs[k][j]) {
+                        costs[i][j] = costs[i][k] + costs[k][j];
+                        predecessors[i][j] = predecessors[k][j];
                     }
                 }
             }
         }
         for (int i = 0; i < costs.length; i++) {
-            for (int j = 0; j < costs[0].length; j++) {
-                if(costs[i][j]==Integer.MIN_VALUE)
-                    return false;
+            if (costs[i][i] < 0) {
+                return false;
             }
         }
         return true;
     }
 
 
-  public boolean is_there_negative_edge(){
-      for (Edge edge : edges) {
-          if (edge.getW() < 0)
-              return true;
-      }
+    public boolean is_there_negative_edge() {
+        for (Edge edge : edges) {
+            if (edge.getW() < 0)
+                return true;
+        }
         return false;
     }
 }
--- src/cli.java
+++ src/cli.java
@@ -7,403 +7,285 @@ import java.util.Scanner;
 import src.CreateGraphs.Graph;
 
 
-
 public class cli {
-    Scanner x =new Scanner(System.in);
-   Graph g;
-    
-  public cli(){
-    Graph gg = new Graph();
-    
-    this.g=gg;
-    this.choose_function_menu();
-   }
-   
+    Scanner x = new Scanner(System.in);
+    Graph g;
 
+    public cli() {
+        Graph gg = new Graph();
 
- void  show_algo_menu(int source){
-    int choice =0;
-
-    while(true){
-
-
-        
-        
-        try{ 
-            System.out.print("List of valid Algorithms\n\n1-Dijkstra\n\n2-Bellman-Ford\n\n3-Floyd-Warshall\n\nEnter your choice (1,2,3) : ");
-             choice = x.nextInt();
-            if(choice==1||choice==2||choice==3){
-                evaluate(choice,source);
-            }
-            else
-            {System.out.print("\n\nInvalid Choice");
-             continue;}
-
-
-            
-         
-
-    
-       
-        break;
-        }catch(InputMismatchException ee){
-            System.out.print("\n\nInvalid Choice");
-            x.next();
-            
-            
-        }
-        
-    
-    
+        this.g = gg;
+        this.choose_function_menu();
     }
-    
-
-   
 
 
+    void show_algo_menu(int source) {
+        int choice = 0;
+
+        while (true) {
 
 
+            try {
+                System.out.print("List of valid Algorithms\n\n1-Dijkstra\n\n2-Bellman-Ford\n\n3-Floyd-Warshall\n\nEnter your choice (1,2,3) : ");
+                choice = x.nextInt();
+                if (choice == 1 || choice == 2 || choice == 3) {
+                    evaluate(choice, source);
+                } else {
+                    System.out.print("\n\nInvalid Choice");
+                    continue;
+                }
 
 
+                break;
+            } catch (InputMismatchException ee) {
+                System.out.print("\n\nInvalid Choice");
+                x.next();
 
-}
-
-
-
-
-
-void  show_algo_menu2(){
-    int choice =0;
-
-    while(true){
-
-
-        
-        
-        try{ 
-            System.out.print("\n\n1List of valid Algorithms\\n\n1-Bellman-Ford\n\n2-Floyd-Warshall\n\nEnter your choice (1,2) : ");
-             choice = x.nextInt();
-            if(choice==1){
-               if(!g.bellmanFord(0,new int[g.size()] , new int[g.size()]))
-                 System.out.println("yes,there is negative cycle");
-               else
-               System.out.println("no,there is not negative cycle");
-               
-            
-
-                
-            }
-            else if(choice==2)
-            {if(!g.Floyd_warshall(new int[g.size()][g.size()] ,new int[g.size()][g.size()]))
-                System.out.println("yes,there is negative cycle");
-                else
-                System.out.println("no,there is not negative cycle");
 
             }
-            else
-            {System.out.print("\n\nInvalid Choice");
-             continue;}
-             choose_function_menu();
 
 
-            
-         
-
-    
-       
-        break;
-        }catch(InputMismatchException ee){
-            System.out.print("\n\nInvalid Choice");
-            x.next();
-            
-            
         }
-        
-    
-    
+
+
     }
-    
+    void show_algo_menu2() {
+        int choice = 0;
+        while (true) {
+            try {
+                System.out.print("\n\n1List of valid Algorithms\\n\n1-Bellman-Ford\n\n2-Floyd-Warshall\n\nEnter your choice (1,2) : ");
+                choice = x.nextInt();
+                if (choice == 1) {
+                    if (!g.bellmanFord(0, new int[g.size()], new int[g.size()]))
+                        System.out.println("yes,there is negative cycle");
+                    else
+                        System.out.println("no,there is not negative cycle");
 
-   
 
+                } else if (choice == 2) {
+                    if (!g.Floyd_warshall(new int[g.size()][g.size()], new int[g.size()][g.size()]))
+                        System.out.println("yes,there is negative cycle");
+                    else
+                        System.out.println("no,there is not negative cycle");
 
+                } else {
+                    System.out.print("\n\nInvalid Choice");
+                    continue;
+                }
+                choose_function_menu();
 
-
-
-
-
-}
-
-
- void choose_source_menu(){
-    while(true){
-        try{
-    System.out.print("\nEnter the source node : ");
-    int source= x.nextInt();
-    if(source>g.size()-1||source<0)
-    {System.out.print("Non Existing Node");
-      continue;}
-      show_algo_menu(source);
-
-    
-      break;
-       
-
-        }catch(InputMismatchException ee){
-            System.out.print("Invalid node name\n");
-            x.next();
+                break;
+            } catch (InputMismatchException ee) {
+                System.out.print("\n\nInvalid Choice");
+                x.next();
+            }
         }
-    
-    
     }
+    void choose_source_menu() {
+        while (true) {
+            try {
+                System.out.print("\nEnter the source node : ");
+                int source = x.nextInt();
+                if (source > g.size() - 1 || source < 0) {
+                    System.out.print("Non Existing Node");
+                    continue;
+                }
+                show_algo_menu(source);
 
 
-        
-}
+                break;
 
 
+            } catch (InputMismatchException ee) {
+                System.out.print("Invalid node name\n");
+                x.next();
+            }
 
 
-
-
-
-
-
- void choose_function_menu(){
-
-
-    int choice =0;
-    while(true){
-        
-        
-        try{ 
-            System.out.print("\n\nList of functionalities\n\n1-Find the shortest path from a source node\n\n2-Find the shortest path between all pairs of nodes\n\n3-check if a negative cycle exist\n\nEnter your choice (1,2,3) : ");
-             choice = x.nextInt();
-            if(choice==1){
-                choose_source_menu();
-                break;}
-             else if(choice==2){
-                show_algo_menu(-1);
-                
-                break;}
-            else if(choice==3){
-                show_algo_menu2();
-                break;}
-            else
-            System.out.print("\n\nInvalid Choice");
-
-
-            
-         
-
-    
-       
-
-        }catch(InputMismatchException ee){
-            System.out.print("\n\nInvalid Choice");
-            x.next();
-            
-            
         }
-        
-    
-    
+
+
     }
-}
+    void choose_function_menu() {
 
 
+        int choice = 0;
+        while (true) {
 
 
- void evaluate(int choice,int source){
-    int[][] costs = new int[g.size()][g.size()];
-    int[][] p = new int[g.size()][g.size()];
+            try {
+                System.out.print("\n\nList of functionalities\n\n1-Find the shortest path from a source node\n\n2-Find the shortest path between all pairs of nodes\n\n3-check if a negative cycle exist\n\nEnter your choice (1,2,3) : ");
+                choice = x.nextInt();
+                if (choice == 1) {
+                    choose_source_menu();
+                    break;
+                } else if (choice == 2) {
+                    show_algo_menu(-1);
 
-    
+                    break;
+                } else if (choice == 3) {
+                    show_algo_menu2();
+                    break;
+                } else
+                    System.out.print("\n\nInvalid Choice");
 
-    
-    if(source==-1)    
-{
-    if(choice==1){
+
+            } catch (InputMismatchException ee) {
+                System.out.print("\n\nInvalid Choice");
+                x.next();
+            }
+        }
+    }
+    void evaluate(int choice, int source) {
+        int[][] costs = new int[g.size()][g.size()];
+        int[][] p = new int[g.size()][g.size()];
+
+
+        if (source == -1) {
+            if (choice == 1) {
 //        if(choice==1&&g.is_there_negative_edge()){
 //            System.out.println("\n\ncannot implement dijkestra as there are negative egdes");
 //            show_algo_menu(source);
 //        }
 //        else{
 
-        for(int i =1;i<=g.size();i++) g.dijkestra(i-1, costs[i-1], p[i-1]);
+                for (int i = 1; i <= g.size(); i++) g.dijkestra(i - 1, costs[i - 1], p[i - 1]);
 
 
-    }
-    else if (choice==2){for(int i =1;i<=g.size();i++)
-        g.bellmanFord(i-1, costs[i-1], p[i-1]);
-    
-    }
-    else if (choice==3){
-        g.Floyd_warshall( costs, p);
-    
-    }
- }
- else { if(choice==1){
+            } else if (choice == 2) {
+                for (int i = 1; i <= g.size(); i++)
+                    g.bellmanFord(i - 1, costs[i - 1], p[i - 1]);
+
+            } else if (choice == 3) {
+                g.Floyd_warshall(costs, p);
+
+            }
+        } else {
+            if (choice == 1) {
 //    if(choice==1&&g.is_there_negative_edge()){
 //        System.out.println("\n\ncannot implement dijkestra as there are negative egdes");
 //        show_algo_menu(source);
 //    }
 //    else{
-    
-    g.dijkestra(source, costs[source], p[source]);
+
+                g.dijkestra(source, costs[source], p[source]);
 
 
-}
-else if (choice==2){
-    g.bellmanFord(source, costs[source], p[source]);
+            } else if (choice == 2) {
+                g.bellmanFord(source, costs[source], p[source]);
 
-}
-else if (choice==3){
-    g.Floyd_warshall( costs, p);
+            } else if (choice == 3) {
+                g.Floyd_warshall(costs, p);
 
-}}
+            }
+        }
 
-    if(source==-1){
-        specific_source_node(source,costs,p);
-        
-         
-        
-
-    }
-    else{
-        
-        specific_destination_node(source,costs,p,0);
-        
-    }
+        if (source == -1) {
+            specific_source_node(source, costs, p);
 
 
+        } else {
 
-}
+            specific_destination_node(source, costs, p, 0);
 
- void specific_source_node(int source,int[][] costs,int[][] p){
-    while(true){
-        try{
-    System.out.print("\n\nEnter Specific Source Node(insert -1 to return to the main menu):");
-    int sc = x.nextInt();
-    if(sc<-1||sc>g.size())
-    {System.out.println("\n\nInvalid");
-     continue;}
+        }
 
-     if(sc==-1)
-     choose_function_menu();
-     
-     else specific_destination_node(sc,costs,p,1);
-break;}
-    catch(InputMismatchException ee){
-        System.out.print("\n\nInvalid Node Try Again");
 
     }
 
+    void specific_source_node(int source, int[][] costs, int[][] p) {
+        while (true) {
+            try {
+                System.out.print("\n\nEnter Specific Source Node(insert -1 to return to the main menu):");
+                int sc = x.nextInt();
+                if (sc < -1 || sc > g.size()) {
+                    System.out.println("\n\nInvalid");
+                    continue;
+                }
 
-}}
- void specific_destination_node(int source,int[][] costs,int[][] p,int is_source_needed){
+                if (sc == -1)
+                    choose_function_menu();
 
- while(true){
-        try{
-    System.out.print("\n\nEnter Specific Destination Node(insert -1 to return to the main menu):");
-    int des = x.nextInt();
-    if(des<-1||des>=g.size())
-    {System.out.println("\n\nInvalid");
-     continue;}
-     if(des==-1)
-     choose_function_menu();
-     
-     
-     else
-     {
-        path_or_cost(source,des,costs,p);
-        if(is_source_needed==1)
-        specific_source_node(-1, costs, p);
-        else specific_destination_node(source, costs, p, is_source_needed);
+                else specific_destination_node(sc, costs, p, 1);
+                break;
+            } catch (InputMismatchException ee) {
+                System.out.print("\n\nInvalid Node Try Again");
+
+            }
 
 
-     }
-
-
-
-break;}
-    catch(InputMismatchException ee){
-        System.out.print("\n\nInvalid Node Try Again");
-
+        }
     }
 
+    void specific_destination_node(int source, int[][] costs, int[][] p, int is_source_needed) {
 
-}}
-
- void path_or_cost (int source,int destination,int[][] costs,int[][] p){
-    while(true){
-        
-    System.out.print("\n\ninsert p for shortest path , c for minimum cost , -1 for returning to the main menu :");
-    String sc = x.next();
-    if(!sc.equals("p")&&!sc.equals("c")&&!sc.equals("-1"))
-    {System.out.println("\n\nInvalid");
-     continue;}
-     if(sc.equals("c"))
-     { if(costs[source][destination]==Integer.MAX_VALUE)
-        System.out.println("\n\nthere is no path from "+source+"to"+destination);
-        else
-        System.out.println("\n\n"+costs[source][destination]);
+        while (true) {
+            try {
+                System.out.print("\n\nEnter Specific Destination Node(insert -1 to return to the main menu):");
+                int des = x.nextInt();
+                if (des < -1 || des >= g.size()) {
+                    System.out.println("\n\nInvalid");
+                    continue;
+                }
+                if (des == -1)
+                    choose_function_menu();
 
 
-     }
-     
-     
-     else if(sc.equals("p"))
-     {  if(costs[source][destination]==Integer.MAX_VALUE)
-        System.out.println("\n\nthere is no path from "+source+"to"+destination);
-        else
-         System.out.println("\n\n"+get_path(source,destination,costs,p));
-
-     }
-     else
-     choose_function_menu();
+                else {
+                    path_or_cost(source, des, costs, p);
+                    if (is_source_needed == 1)
+                        specific_source_node(-1, costs, p);
+                    else specific_destination_node(source, costs, p, is_source_needed);
 
 
+                }
+                break;
+            } catch (InputMismatchException ee) {
+                System.out.print("\n\nInvalid Node Try Again");
+
+            }
+        }
+    }
+
+    void path_or_cost(int source, int destination, int[][] costs, int[][] p) {
+        while (true) {
+
+            System.out.print("\n\ninsert p for shortest path , c for minimum cost , -1 for returning to the main menu :");
+            String sc = x.next();
+            if (!sc.equals("p") && !sc.equals("c") && !sc.equals("-1")) {
+                System.out.println("\n\nInvalid");
+                continue;
+            }
+            if (sc.equals("c")) {
+                if (costs[source][destination] == Integer.MAX_VALUE)
+                    System.out.println("\n\nthere is no path from " + source + "to" + destination);
+                else
+                    System.out.println("\n\n" + costs[source][destination]);
 
 
-    
+            } else if (sc.equals("p")) {
+                if (costs[source][destination] == Integer.MAX_VALUE)
+                    System.out.println("\n\nthere is no path from " + source + "to" + destination);
+                else
+                    System.out.println("\n\n" + get_path(source, destination, costs, p));
 
-  break;
-}
-
-
-
-
-
-}
-
- String get_path(int source,int destination,int[][] costs,int[][] p){
-    
-    
-    
-    if(source==destination)
-    return  String.valueOf(source);
-    else return destination+"<="+get_path(source,p[source][destination],costs,p);
-    
-    
-}
+            } else
+                choose_function_menu();
 
 
+            break;
+        }
+    }
+
+    String get_path(int source, int destination, int[][] costs, int[][] p) {
 
 
-
-    
-    
-
-
+        if (source == destination)
+            return String.valueOf(source);
+        else return destination + "<=" + get_path(source, p[source][destination], costs, p);
 
 
-
-
-
-
-
-
+    }
 
 
 }
