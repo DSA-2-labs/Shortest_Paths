5113268986e63c8c8b7151b99bb76580bd4a6a0c
AhmedAli3011 <128240330+AhmedAli3011@users.noreply.github.com>
2023-04-29 13:24:45 +0200

    Add files via upload

--- src/CreateGraphs/Graph.java
+++ src/CreateGraphs/Graph.java
@@ -6,10 +6,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Graph {
-    private static final int INF = Integer.MAX_VALUE;
     private int V,E;
     private Edge[] edges;
     private int[][] adjMatrix;
+
+
+
+
+
+
+
+
     public Graph(String filename)
     {
         try {
@@ -34,6 +41,53 @@ public class Graph {
             e.printStackTrace();
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+    public Graph()
+    {  Scanner input = new Scanner(System.in);
+       
+        while(true)
+        { int Error=0;
+            System.out.print("Enter the path of the File that contain the Graph : ");
+          String s = input.nextLine();
+        try {
+            Scanner scan=new Scanner(new File(s));
+            V=scan.nextInt();
+            E= scan.nextInt();
+            edges=new Edge[E];
+            adjMatrix= new int[V][V];
+            for (int i = 0; i < E; i++) {
+                edges[i]=new Edge(scan.nextInt(),scan.nextInt(),scan.nextInt());
+                int x = edges[i].getFrom();
+                int y = edges[i].getTo();
+                int w = edges[i].getW();
+                adjMatrix[x][y] = w;
+                adjMatrix[x][x] = 0;
+                adjMatrix[y][y] = 0;
+                Error=0;
+            }
+            scan.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.print("\nOops Wrong path \n"); 
+            Error=1;
+
+        } 
+        if(Error==0){
+            break;
+        }
+    }
+    }
     public int size()
     {
         return V;
@@ -46,7 +100,7 @@ public class Graph {
         }
     }
     public boolean bellmanFord(int src,int[] cost,int[] p){
-        Arrays.fill(cost, INF);
+        Arrays.fill(cost,Integer.MAX_VALUE);
         Arrays.fill(p,-1);
         cost[src] = 0;
         for (int i = 0; i <V-1 ; i++) {
@@ -54,8 +108,8 @@ public class Graph {
                 int u = edges[j].getFrom();
                 int v = edges[j].getTo();
                 int w = edges[j].getW();
-                if (cost[u]!=INF && cost[u]+w<cost[v]){
-                    cost[v] = Math.max(-INF,cost[u]+w);//for overflow
+                if (cost[u]!=Integer.MAX_VALUE && cost[u]+w<cost[v]){
+                    cost[v] = Math.max(-Integer.MAX_VALUE,cost[u]+w);//for overflow
                     p[v] = u;
                 }
             }
@@ -65,7 +119,7 @@ public class Graph {
             int u = edges[i].getFrom();
             int v = edges[i].getTo();
             int w = edges[i].getW();
-            if (cost[u]!=INF && cost[u]+w<cost[v]){
+            if (cost[u]!=Integer.MAX_VALUE && cost[u]+w<cost[v]){
                 return false;
             }
         }
@@ -74,19 +128,18 @@ public class Graph {
     public void dijkestra(int src, int[] cost, int[] p){
         boolean[] added = new boolean[V];
         for (int i = 0; i < V; i++) {
-            cost[i] = INF;
+            cost[i] = Integer.MAX_VALUE;
             added[i] = false;
         }
         cost[src] = 0;
         p[src] = -1;
         for(int i=0 ; i<V ; i++){
             int nearestV = -1;
-            int shortest_D = INF;
+            int shortest_D = Integer.MAX_VALUE;
             for(int j=0 ; j<V ; j++){
                 if(!added[j] && cost[j] < shortest_D){
                     shortest_D = cost[j];
                     nearestV = j;
-                    System.out.println(V+" "+nearestV);
                 }
             }
             added[nearestV] = true;
@@ -102,33 +155,42 @@ public class Graph {
         }
     }
 
-    public boolean Floyd_warshall(int[][] costs,int[][] predecessors) {
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if(i == j) costs[i][j] = 0;
-                else costs[i][j] = INF;
-                predecessors[i][j] = -1;
-            }
+    public boolean Floyd_warshall(int[][] costs,int[][] predecessors){
+        for (int i = 0; i < predecessors.length; i++) {
+            Arrays.fill(predecessors[i],Integer.MAX_VALUE);
+            predecessors[i][i]=0;
         }
-        for(Edge edge: edges) {
-            costs[edge.getFrom()][edge.getTo()] = edge.getW();
-            predecessors[edge.getFrom()][edge.getTo()] = edge.getFrom();
+        for (int i = 0; i < E; i++) {
+            predecessors[edges[i].getFrom()][edges[i].getTo()]=edges[i].getW();
         }
-        for (int k = 0; k < V; k++) {
-            for (int i = 0; i < V; i++) {
-                for (int j = 0; j < V; j++) {
-                    if (costs[i][k] != INF && costs[k][j] != INF && costs[i][j] > costs[i][k]+costs[k][j]) {
-                        costs[i][j]=costs[i][k]+costs[k][j];
-                        predecessors[i][j] = predecessors[k][j];
+        costs=predecessors;
+        for (int k = 0; k < adjMatrix.length; k++) {
+            for (int i = 0; i < adjMatrix.length; i++) {
+                for (int j = 0; j < adjMatrix.length; j++) {
+                    if (costs[i][k] != Integer.MAX_VALUE && costs[k][j] != Integer.MAX_VALUE)//for overflow
+                    {
+                        costs[i][j]=Math.min(costs[i][j],costs[i][k]+costs[k][j]);
+                        if (costs[k][j] < Integer.MAX_VALUE && costs[j][j] < 0 && costs[j][i] < Integer.MAX_VALUE)
+                            costs[k][i] = Integer.MIN_VALUE;
                     }
                 }
             }
         }
         for (int i = 0; i < costs.length; i++) {
-            if(costs[i][i] < 0) {
-                return false;
+            for (int j = 0; j < costs[0].length; j++) {
+                if(costs[i][j]==Integer.MIN_VALUE)
+                    return false;
             }
         }
         return true;
+    }
+
+
+  public boolean is_there_negative_edge(){
+        for(int i =0;i<edges.length;i++){
+            if(edges[i].getW()<0)
+             return true;
+        }
+        return false;
     }
 }
